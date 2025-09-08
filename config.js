4db7efa3fd2060eb346e01381b775ae71c9384c2
Levanter-Originals <levanteroriginals@gmail.com>
2025-09-08 22:42:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "atIHGKwT#wQFzz6IEdLaunqUFRqDTtfns0lLsLhcLS8XLrzoeGO4",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+BOT_OWNER: '94720522884',  // Replace with the owner's phone number
 
 
 
