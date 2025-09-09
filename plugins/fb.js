43012251db1f209a24a475db0a9d535a541160c0
Levanter-Originals <levanteroriginals@gmail.com>
2025-09-09 08:33:40 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -5,7 +5,7 @@ cmd(
   {
     pattern: "fb",
     alias: ["facebook"],
-    react: "📍",
+    react: "✅",
     desc: "Download Facebook Video",
     category: "download",
     filename: __filename,
@@ -40,28 +40,45 @@ cmd(
     }
   ) => {
     try {
-      if (!q) return reply("Please provide a valid Facebook video URL! ❤");
+      if (!q) return reply("*Please provide a valid Facebook video URL!* ❤️");
 
       const fbRegex = /(https?:\/\/)?(www\.)?(facebook|fb)\.com\/.+/;
       if (!fbRegex.test(q))
-        return reply("Invalid Facebook URL! Please check and try again. ☹");
+        return reply("*Invalid Facebook URL! Please check and try again.* ☹️");
 
-      reply("Downloading your video... ❤");
+      reply("*Downloading your video...* ❤️");
 
       const result = await getFbVideoInfo(q);
       if (!result || (!result.sd && !result.hd)) {
-        return reply("Failed to download video. Please try again later. ☹");
+        return reply("*Failed to download video. Please try again later.* ☹️");
       }
 
       const { title, sd, hd } = result;
       const bestQualityUrl = hd || sd;
       const qualityText = hd ? "HD" : "SD";
 
+      const desc = `
+Your fb video
+👻 *Title*: ${title || "Unknown"}
+👻 *Quality*: ${qualityText}
+`;
+
+      await danuwa.sendMessage(
+        from,
+        {
+          image: {
+            url: "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/fbdownloader.png?raw=true",
+          },
+          caption: desc,
+        },
+        { quoted: mek }
+      );
+
       await danuwa.sendMessage(
         from,
         {
           video: { url: bestQualityUrl },
-          caption: *📥 Downloaded in ${qualityText} quality*,
+          caption: `*📥 Downloaded in ${qualityText} quality*`,
         },
         { quoted: mek }
       );
@@ -69,7 +86,7 @@ cmd(
       return reply("Thank you for using DANUWA-MD");
     } catch (e) {
       console.error(e);
-      reply(*Error:* ${e.message || e});
+      reply(`*Error:* ${e.message || e}`);
     }
   }
 );
